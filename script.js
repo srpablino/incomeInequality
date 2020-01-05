9812bf5c693fae98fb9f14837a555d3089699f38
srpablino <srpablino@gmail.com>
2020-01-05 18:46:20 +0100

    Interactions working

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var data, columns;
 var table;
 var thead;
 var tbody;
+var checkedIndicators = []
+var checkedCountries = []
 
 var tabulate = function () {
 	table = d3.select('#d3table').append('table')
@@ -12,6 +14,9 @@ var tabulate = function () {
 	  .selectAll('th')
 	    .data(columns)
 	    .enter()
+		.filter(function (d,i) {
+			return checkedIndicators.includes(d) || i < 3
+		})
 	  .append('th')
 	    .text(function (d, i) {
 	    	return d
@@ -22,36 +27,43 @@ var tabulate = function () {
 	    .enter()
 		.append('tr')
 		.filter(function(d) {
-			return d.year == 1900 + (sliderTime.value().getYear())
+			return d.year == 1900 + (sliderTime.value().getYear()) && checkedCountries.includes(d.name)
 		})
-	filtered = rows.data();
-	var maxmin = getMaxMin(filtered);
 
-	var cells = rows.selectAll('td')
-	    .data(function(row) {
-	    	return columns.map(function (column) {
-	    		return { column: column, value: row[column] }
-	      })
-      })
-      .enter()
-    .append('td')
-		.text(function (d) { return  isNaN(d.value)? d.value : Math.round(d.value * 100) / 100  })
+	if (checkedCountries.length > 0) {
+		filtered = rows.data();
+		var maxmin = getMaxMin(filtered);
 
-	table.selectAll("td")
-		.style("background", function(d,i) {
-			if (!isNaN(d.value)){
-				if (i % columns.length > 2)
-					return "background", "linear-gradient(to right,  red "+d.value+"%, red "+(100 - d.value)+"%, white "+(100 - d.value)+"%)"
-				if (i % columns.length == 2){
-					//console.log(normalize(maxmin,d.value)*100)
-					//console.log(100 - normalize(maxmin,d.value)*100)
-					return "background", "linear-gradient(to right,  blue "+normalize(maxmin,d.value)*100
-					+"%,  blue "+ (100 - normalize(maxmin,d.value)*100)
-					+"%,  white "+ (100 - normalize(maxmin,d.value)*100)+"%)"
+		var cells = rows.selectAll('td')
+			.data(function (row) {
+				return columns.map(function (column) {
+					return {column: column, value: row[column]}
+				})
+			})
+			.enter()
+			.filter(function (d, i) {
+				return checkedIndicators.includes(d.column) || i < 3;
+			})
+			.append('td')
+
+			.text(function (d) {
+				return isNaN(d.value) ? d.value : Math.round(d.value * 100) / 100
+			})
+
+		table.selectAll("td")
+			.style("background", function (d, i) {
+				if (!isNaN(d.value)) {
+					if (i % (checkedIndicators.length + 3) > 2)
+						return "background", "linear-gradient(to right,  red " + d.value + "%, red " + (100 - d.value) + "%, white " + (100 - d.value) + "%)"
+					if (i % (checkedIndicators.length + 3) == 2 || i == 2) {
+						return "background", "linear-gradient(to right,  blue " + normalize(maxmin, d.value) * 100
+						+ "%,  blue " + (100 - normalize(maxmin, d.value) * 100)
+						+ "%,  white " + (100 - normalize(maxmin, d.value) * 100) + "%)"
+					}
+
 				}
-
-			}
-		})
+			})
+	}
 	return table;
 }
 
@@ -73,7 +85,7 @@ var indicatorSelector = function(){
 
 d3.csv('data.csv')
 	.then(function(d) {
-		columns = ['name','year','InequalityRate','property rights','government integrity','judicial effectiveness','tax burden']
+		columns = ['name',	'year',	'InequalityRate',	'property rights',	'government integrity',	'judicial effectiveness',	'tax burden',	'government spending',	'fiscal health',	'business freedom',	'labor freedom',	'monetary freedom',	'trade freedom',	'investment freedom',	'financial freedom']
 		data = d;
 		tabulate();
 		countriesSelector();
@@ -112,15 +124,15 @@ gTime.call(sliderTime);
 //d3.select('p#value-time').text(d3.timeFormat('%Y')(sliderTime.value()));
 
 //utils
-var getMaxMin = function (data){
-	var max = parseFloat(data[0]["InequalityRate"])
-	var min = parseFloat(data[0]["InequalityRate"])
-	for (i=0; i<data.length;i++){
-		if (parseFloat(data[i]["InequalityRate"]) > max){
-			max = parseFloat(data[i]["InequalityRate"]);
+var getMaxMin = function (d){
+	var max = parseFloat(d[0]["InequalityRate"])
+	var min = parseFloat(d[0]["InequalityRate"])
+	for (i=0; i<d.length;i++){
+		if (parseFloat(d[i]["InequalityRate"]) > max){
+			max = parseFloat(d[i]["InequalityRate"]);
 		}
-		if (parseFloat(data[i]["InequalityRate"]) < min){
-			min = parseFloat(data[i]["InequalityRate"]);
+		if (parseFloat(d[i]["InequalityRate"]) < min){
+			min = parseFloat(d[i]["InequalityRate"]);
 		}
 	}
 	return [max,min]
@@ -143,6 +155,21 @@ var radioButtonsCountries = function(selections){
 		.text(function(d) { return d; })
 		.insert("input")
 		.attr("type", "checkbox")
+		.attr("name","countriesCheck")
+
+	ul.selectAll("input").on("click", function(val) {
+		if (this.checked){
+			if (checkedCountries.length > 14){
+				this.checked = false
+			}else{
+				checkedCountries.push(val);
+			}
+		}else{
+			checkedCountries = checkedCountries.filter(item => item !== val)
+		}
+		table.remove();
+		tabulate()
+	});
 }
 
 var radioButtonsIndicators = function(selections){
@@ -156,5 +183,20 @@ var radioButtonsIndicators = function(selections){
 		.append("label")
 		.text(function(d) { return d; })
 		.insert("input")
+		.attr("name","indicatorsCheck")
 		.attr("type", "checkbox")
+
+	ul.selectAll("input").on("click", function(val) {
+		if (this.checked){
+			if (checkedIndicators.length > 4){
+				this.checked = false
+			}else{
+				checkedIndicators.push(val);
+			}
+		}else{
+			checkedIndicators = checkedIndicators.filter(item => item !== val)
+		}
+		table.remove();
+		tabulate()
+	});
 }
